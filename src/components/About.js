231e4bca23c624870cbf3b9be61b59158c3aab67
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-03-13 17:43:08 -0400

    new edits

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,26 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Cameron.
+            <br className="hidden lg:inline-block" />Crafting innovative solutions for complex challenges..
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          Welcome to my portfolio! I'm a software engineer by major, and I have a firm foundation 
+          in project management, UI/UX design, and software development. I'm passionate about using 
+          technology to solve challenging issues and spur creativity,
+           and I do best in collaborative settings where I can closely 
+           collaborate with customers and development teams to produce 
+           top-notch outcomes. I bring a distinctive viewpoint and a 
+           wide variety of talents to any project I take on because of 
+           my experience in both academic and professional settings. 
+           I appreciate you taking the time to browse at my portfolio,
+            and I'm excited to show you my work.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              Lets Connect ☕️
             </a>
             <a
               href="#projects"
