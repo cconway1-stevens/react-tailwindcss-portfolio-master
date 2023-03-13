2661a742874f89960effceda9ea9673ef120b247
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-03-13 18:28:53 -0400

    new about us page

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,18 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Cameron.
-            <br className="hidden lg:inline-block" />Crafting innovative solutions for complex challenges..
+            Hi, I'm Cameron 👋
+            <br className="hidden lg:inline-block" />Here to Crafting Innovative Solutions
           </h1>
           <p className="mb-8 leading-relaxed">
-          Welcome to my portfolio! I'm a software engineer by major, and I have a firm foundation 
-          in project management, UI/UX design, and software development. I'm passionate about using 
-          technology to solve challenging issues and spur creativity,
-           and I do best in collaborative settings where I can closely 
-           collaborate with customers and development teams to produce 
-           top-notch outcomes. I bring a distinctive viewpoint and a 
-           wide variety of talents to any project I take on because of 
-           my experience in both academic and professional settings. 
-           I appreciate you taking the time to browse at my portfolio,
-            and I'm excited to show you my work.
+          I'm a software engineer with a background in project management, 
+          UI/UX design, and software development. I'm passionate about using 
+          technology to tackle complex problems and drive innovation. I thrive
+          in collaborative environments, working closely with clients and development
+          teams to deliver high-quality results. With experience in both academic
+          and professional settings, I bring a unique perspective and diverse skill
+          set to every project. Thank you for visiting my portfolio, 
+          and I look forward to showcasing my work to you.
           </p>
           <div className="flex justify-center">
             <a
