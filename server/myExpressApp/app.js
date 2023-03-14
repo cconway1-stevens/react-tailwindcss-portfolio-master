735b5b0f5faad10a49c185e69fe66c94cd850723
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-03-14 00:06:51 -0400

    .env hidden file

--- server/myExpressApp/app.js
+++ server/myExpressApp/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,6 +40,47 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+
+// connect to MongoDB Atlas
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// define a schema for your MongoDB collection
+const formSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String
+});
+
+// define a model for your MongoDB collection
+const Form = mongoose.model('Form', formSchema);
+
+// parse incoming request bodies as JSON
+app.use(bodyParser.json());
+
+// define a POST route for form submissions
+app.post('/submit-form', async (req, res) => {
+  try {
+    // create a new document in the 'forms' collection using the submitted data
+    const form = new Form(req.body);
+    await form.save();
+
+    // send a success response
+    res.status(200).json({ message: 'Form submitted successfully!' });
+  } catch (error) {
+    // handle errors
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
 
 module.exports = app;
