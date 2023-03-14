915f72292aa79c30c144b6749c1e815c78292b8e
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-03-13 22:17:29 -0400

    New Branch Seed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+// import ChatBot from 'react-simple-chatbot';
+
+// const steps = [
+//   {
+//       id: '0',
+//       message: 'Hey Geek!',
+//       end: true
+//   }
+// ];
 
 export default function App() {
   return (
+    
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
@@ -15,6 +25,11 @@ export default function App() {
       <Skills />
       {/*<Testimonials />*/}
       <Contact />
+      <chatbot />
+      {/* <ChatBot steps={steps} /> */}
     </main>
+      
   );
 }
+
+
